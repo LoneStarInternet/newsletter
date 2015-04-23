dc0eb4e274ae78473855c4b733f8808ecb92ab2b
Christopher Louis Hauboldt <chris@hauboldt.us>
2015-04-23 05:23:32 -0500

    asset factory fix to work outside of gem

--- spec/test_app/spec/factories/assets.rb
+++ spec/test_app/spec/factories/assets.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :asset, class: Newsletter::Asset do
     #association :field
     #association :piece
-    image File.open(File.join(Rails.root, 
-      '/spec/support/files/iReach_logo.gif'))
+    image File.open(File.join(Newsletter::PLUGIN_ROOT,'spec','test_app',
+      'spec','support','files','iReach_logo.gif'))
   end
 end
